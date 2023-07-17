6f07d8646530fa33350efd72443b7f5912623afa
Faris <faris.shariff@glider.ai>
2023-07-17 15:21:54 +0530

    Minimized Form Parameters.

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.post('/oAuth', async (req, res) => {
 
 app.post('/', (req, res) => {
   var extraParams = req.body;
+  console.log(extraParams);
   var OAuth = require("oauth");
 
   var oauthUrl = "http://localhost:8080/oAuth";
@@ -60,36 +61,7 @@ app.post('/', (req, res) => {
 
   var method = "post", url = oauthUrl;
 
-  extraParams = {
-    context_id: 'uYwvxnvGKFnAxvSYo',
-  context_title: 'xc%20xc',
-  custom_assessment_id: '257078',
-  custom_assignment_author: 'GduqyQajsx3krzHap',
-  custom_assignment_params: '{"isProctored":false,"validTill":1689572049976,"allAttemptedQuestionIds":[]}',
-  custom_course_name: 'g4GZp8xa9rnbbQwbu_all_course',
-  custom_courseid: 'g4GZp8xa9rnbbQwbu_all',
-  custom_district_id: 'g4GZp8xa9rnbbQwbu',
-  custom_domainid: 'g4GZp8xa9rnbbQwbu',
-  custom_due_date: '1719566141901',
-  custom_effective_date: '1687943741901',
-  custom_grade: 'all',
-  custom_subject: 'all',
-  import_report_flow: 'IMPORT_FLOW',
-  launch_presentation_document_target: 'frame',
-  launch_presentation_return_url: 'http://glider.com/userProfile',
-  lis_outcome_service_url: 'http://localhost:3000/api/update/assignment/result',
-  lis_person_contact_email_primary: 'jdb@bv.in',
-  lis_person_name_family: '%20',
-  lis_person_name_given: 'Fris',
-  lti_message_type: 'takeAssignment',
-  lti_version: 'LTI-1p0',
-  roles: 'Student',
-  tool_consumer_info_product_family_code: 'glider',
-  tool_consumer_instance_guid: 'com.gliderapp.snapwiz',
-  tool_consumer_instance_name: 'Edulastic',
-  user_id: '3f6viixLSHgB8tm6m'
-  }
-  console.log({extraParams});
+  // console.log({extraParams});
 
   var orderedParams = oauth._prepareParameters("", "", method, url, extraParams);
   var eparams = [];
@@ -101,7 +73,6 @@ app.post('/', (req, res) => {
       eparams.push(obj);
   });
   // console.log({eparams});
-  // console.log("Hits");
   // res.json({ status: 'success', message: 'Data received successfully!', params : eparams });
   res.render('params', { params: eparams });
 });
